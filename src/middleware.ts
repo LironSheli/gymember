9053ec894c1c818c8d2bd6ab51552dad12f6ff9e
Liron <brightsitewebsite@gmail.com>
2025-06-22 04:57:23 +0300

    Temp

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,5 +26,14 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: "/api/:path*",
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     */
+    "/((?!_next/static|_next/image|favicon.ico|public).*)",
+  ],
 };
